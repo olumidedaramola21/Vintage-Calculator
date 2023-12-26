926d64fbae801d0987ed4ffd36d286283e8920e0
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-26 16:51:04 +0100

    feat: buttons work haha

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
+import  { useState } from 'react';
 
-import './App.css'
+import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const handleClear = () => setInput('');
+
+  const handleAppend = (value) => {
+    setInput((prev) => prev + value);
+  };
 
   return (
    <div className='container'>
     <form action="" name='calc'  className='calculator'>
-      <input type="text" className='value' readOnly />
-      <span className='num  clear' onClick="calc.txt.value"><i>C</i></span>
-      <span className='num' onClick="calc.txt.value+='/'"><i>/</i></span>
-      <span className='num' onClick="calc.txt.value+='*'"><i>*</i></span>
-      <span className='num' onClick="calc.txt.value+='7'"><i>7</i></span>
-      <span className='num' onClick="calc.txt.value+='8'"><i>8</i></span>
-      <span className='num' onClick="calc.txt.value+='9'"><i>9</i></span>
-      <span className='num' onClick="calc.txt.value+='-'"><i>-</i></span>
-      <span className='num' onClick="calc.txt.value+='4'"><i>4</i></span>
-      <span className='num' onClick="calc.txt.value+='5'"><i>5</i></span>
-      <span className='num' onClick="calc.txt.value+='6'"><i>6</i></span>
-      <span className='num plus' onClick="calc.txt.value+='+'"><i>+</i></span>
-      <span className='num' onClick="calc.txt.value+='1'"><i>1</i></span>
-      <span className='num' onClick="calc.txt.value+='2'"><i>2</i></span>
-      <span className='num' onClick="calc.txt.value+='3'"><i>3</i></span>
-      <span className='num' onClick="calc.txt.value+='0'"><i>0</i></span>
-      <span className='num' onClick="calc.txt.value+='00'"><i>00</i></span>
-      <span className='num' onClick="calc.txt.value+='.'"><i>.</i></span>
+      <input type="text" className='value' value={input} readOnly  />
+      <span className='num  clear' onClick={() => handleClear()}><i>C</i></span>
+      <span className='num' onClick={() => handleAppend('/')}><i>/</i></span>
+      <span className='num' onClick={() => handleAppend('*')}><i>*</i></span>
+      <span className='num' onClick={() => handleAppend('7')}><i>7</i></span>
+      <span className='num' onClick={() => handleAppend('8')}><i>8</i></span>
+      <span className='num' onClick={() => handleAppend('9')}><i>9</i></span>
+      <span className='num' onClick={() => handleAppend('-')}><i>-</i></span>
+      <span className='num' onClick={() => handleAppend('4')}><i>4</i></span>
+      <span className='num' onClick={() => handleAppend('5')}><i>5</i></span>
+      <span className='num' onClick={() => handleAppend('6')}><i>6</i></span>
+      <span className='num plus' onClick={() => handleAppend('+')}><i>+</i></span>
+      <span className='num' onClick={() => handleAppend('1')}><i>1</i></span>
+      <span className='num' onClick={() => handleAppend('2')}><i>2</i></span>
+      <span className='num' onClick={() => handleAppend('3')}><i>3</i></span>
+      <span className='num' onClick={() => handleAppend('0')}><i>0</i></span>
+      <span className='num' onClick={() => handleAppend('00')}><i>00</i></span>
+      <span className='num' onClick={() => handleAppend('.')}><i>.</i></span>
 
 
       <span className='num equal' onClick="document.calc.txt.value=eval(calc.txt.value) "><i>=</i></span>
